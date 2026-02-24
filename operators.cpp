03d33d50c6af93ee253a9bc9591fd472c6eef70e
Mehakpreet kaur <mehakpreetgosal@gmail.com>
2026-02-24 16:38:32 +0530

    solving problems via operators
    Revise operators and implement them while solving problems. Includes mean calculation using relational and assignment operators.

---
+++ operators.cpp
@@ -0,0 +1,15 @@
+// in this file , i am revising operators and implementing them while solving problems
+#include <iostram.h>
+int main()
+{ 
+  int arr[10] = {1,2,3,4,5,6,7,8,9}; //this is an array
+  for(int num = 0; num<=9; num+=2)
+    cout << " using relational and assignment operators to do this"<<endl;
+  // finding mean of some values using operators
+  int a = 67;
+  float b = 59.66;
+  float sum = a + b;
+  float mean = sum /2;
+  cout<<mean;
+  
+}
